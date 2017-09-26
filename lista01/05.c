2c4f4f94331702256f7a3606f0ab3f833df71b5d
kennedybonfim <kennedygbonfim@gmail.com>
2017-09-26 12:19:28 -0300

    Exercicio 05 da lista01

---
+++ lista01/05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+	int x, y;
+	printf("Entre com um valor inteiro x: ");
+	scanf("%d",&x);
+
+	printf("Entre com um valor inteiro y: ");
+	scanf("%d",&y);
+	
+	printf("A divisão dos inteiro x pelo inteiro y é igual a %d\n",x/y);	
+
+	return 0;
+}
