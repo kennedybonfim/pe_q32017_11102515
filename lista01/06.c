08760914ed06683ac5d9933e2d28f9b2e9904120
kennedybonfim <kennedygbonfim@gmail.com>
2017-09-26 14:08:40 -0300

    Exercicio 06 da lista01

---
+++ lista01/06.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+	int x,contador,fatorial;
+	printf("Entre com um valor inteiro x: ");
+	scanf("%d",&x);
+	
+	contador = x;
+	fatorial = x;
+	
+	for ( int i = 1; i < contador; i++){
+	
+		fatorial*= (x-1);
+		--x;
+
+	}
+	
+	printf("O fatorial de %d é igual a %d\n",contador,fatorial);	
+
+	return 0;
+}
