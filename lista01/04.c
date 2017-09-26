5fa6278b6f59fa4c98ad974d3d438d5c212779af
kennedybonfim <kennedygbonfim@gmail.com>
2017-09-26 12:16:46 -0300

    Exercicio 04 da lista01

---
+++ lista01/04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+	int x, y;
+	printf("Entre com um valor inteiro x: ");
+	scanf("%d",&x);
+
+	printf("Entre com um valor inteiro y: ");
+	scanf("%d",&y);
+	
+	printf("A multiplicação dos inteiros x e y é igual a %d\n",x*y);	
+
+	return 0;
+}
