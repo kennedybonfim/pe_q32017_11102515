b5f4b6eef76543f694008492c041ce8967354ae8
kennedybonfim <kennedygbonfim@gmail.com>
2017-10-02 17:12:17 -0300

    Exercicio 16 da lista01

---
+++ lista01/16.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+
+int main(){
+
+	int idCargo, faltas, horasExtras;
+	float salarioBase, descontos, acrescimos;
+
+	printf("Entre o id do cargo: ");
+	scanf("%d",&idCargo);
+
+	printf("Entre o numero de faltas: ");
+	scanf("%d",&faltas);
+
+	printf("Entre o numero de horas extras: ");
+	scanf("%d",&horasExtras);
+
+	switch (idCargo){
+
+	case 1:
+		salarioBase = 10000;
+		descontos = faltas*(salarioBase/20);
+		acrescimos = horasExtras*((salarioBase/160)+40);
+		salarioBase = salarioBase+acrescimos-descontos;
+
+		printf("Cargo         : Diretor\n# de faltas   : %d\n# horas-extras: %d\n",faltas,horasExtras);
+		printf("Descontos     : %.1f\nAcrescismos   : %.1f\nSalario       : %.1f\n",descontos,acrescimos,salarioBase);
+
+	case 2:
+		salarioBase = 8000;
+		descontos = faltas*(salarioBase/20);
+		acrescimos = horasExtras*((salarioBase/160)+40);
+		salarioBase = salarioBase+acrescimos-descontos;
+
+		printf("Cargo         : Gerente\n# de faltas   : %d\n# horas-extras: %d\n",faltas,horasExtras);
+		printf("Descontos     : %.1f\nAcrescismos   : %.1f\nSalario       : %.1f\n",descontos,acrescimos,salarioBase);
+
+	case 3:
+		salarioBase = 5000;
+		descontos = faltas*(salarioBase/20);
+		acrescimos = horasExtras*((salarioBase/160)+40);
+		salarioBase = salarioBase+acrescimos-descontos;
+
+		printf("Cargo         : Engenheiro\n# de faltas   : %d\n# horas-extras: %d\n",faltas,horasExtras);
+		printf("Descontos     : %.1f\nAcrescismos   : %.1f\nSalario       : %.1f\n",descontos,acrescimos,salarioBase);
+
+	case 4:
+		salarioBase = 3000;
+		descontos = faltas*(salarioBase/20);
+		acrescimos = horasExtras*((salarioBase/160)+40);
+		salarioBase = salarioBase+acrescimos-descontos;
+
+		printf("Cargo         : Tecnico\n# de faltas   : %d\n# horas-extras: %d\n",faltas,horasExtras);
+		printf("Descontos     : %.1f\nAcrescismos   : %.1f\nSalario       : %.1f\n",descontos,acrescimos,salarioBase);
+
+ 	case 5:
+		salarioBase = 2000;
+		descontos = faltas*(salarioBase/20);
+		acrescimos = horasExtras*((salarioBase/160)+40);
+		salarioBase = salarioBase+acrescimos-descontos;
+
+		printf("Cargo         : Operador\n# de faltas   : %d\n# horas-extras: %d\n",faltas,horasExtras);
+		printf("Descontos     : %.1f\nAcrescismos   : %.1f\nSalario       : %.1f\n",descontos,acrescimos,salarioBase);
+
+	break;
+
+	default:
+		printf ("Valor invalido!\n");
+
+	}
+
+	return 0;
+
+}
