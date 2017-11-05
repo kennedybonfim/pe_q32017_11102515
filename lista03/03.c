902ca24d606deb7b641b8f778e8d837e43d1438b
kennedybonfim <kennedygbonfim@gmail.com>
2017-11-05 18:15:25 -0200

    Exercicio 3 da lista03

---
+++ lista03/03.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *leVetor(int *v, int n)
+{
+	for(int i=0;i<n;i++) scanf("%d",&v[i]);
+	return v;
+}
+
+void somaVetor(int *v1,int *v2, int n)
+{
+	int v3[n];
+	
+	for(int i=0;i<n;i++) 
+	{
+		v3[i] = v1[i]+v2[i];
+		printf("%d ",v3[i]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	unsigned int i=5;
+	int v1[i],v2[i];
+
+	leVetor(v1,i);
+	leVetor(v2,i);
+	somaVetor(v1,v2,i);
+
+}
+
